28c2f91fcb9b23c04793164164e3ea26b456cfba
snowyy16 <huynhson532004@gmail.com>
2024-12-05 18:21:04 +0700

    weather app js

--- constants.js
+++ constants.js
@@ -1,3 +1,37 @@
-export const TYPE_LOG = 'log'
-export const TYPE_WARN = 'warn'
-export const TYPE_ERROR = 'error'
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    const timeOut = setTimeout(fn,t,...args)
+    
+    return function(){
+        clearTimeOut(timeOut)
+    }
+};
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 5;
+ *  const args = [2], t = 20, cancelTimeMs = 50;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelTimeMs);
+ *           
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
